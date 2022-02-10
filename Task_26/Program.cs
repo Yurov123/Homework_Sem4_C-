0ea6c3e351f7f826ddb4e5e5bcea304330d3586c
Konstantin <constantin8271@gmail.com>
2022-02-10 22:18:57 +0300

    task_26

---
+++ Task_26/Program.cs
@@ -0,0 +1,32 @@
+﻿// Возведите число А в натуральную степень B, используя цикл.
+
+ 
+  Console.Write("Enter number : "); //число, которое нужно возвести в степень
+  int num = int.Parse(Console.ReadLine());
+  Console.Write("Enter degree : ");  //степень числа
+  int n = int.Parse(Console.ReadLine());
+  int num_n = 1; //число, возведенное в степень
+
+  for(int i = 0; i < n; i++) 
+  {
+   num_n *= num;
+  }
+  Console.WriteLine("{0} ^ {1} = {2}", num, n, num_n);
+  return 0;
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
