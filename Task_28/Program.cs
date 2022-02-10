829157aca502f02e0f17de028d131fae8eb73f13
Konstantin <constantin8271@gmail.com>
2022-02-10 22:48:17 +0300

    task_28

---
+++ Task_28/Program.cs
@@ -0,0 +1,22 @@
+﻿// Подсчитайте сумму цифр в числе.
+
+    int a = int.Parse(Console.ReadLine());
+    int s = 0;
+    while (a > 0)
+    {
+      s = s + a % 10;
+      a = a /10 ;
+    }
+ Console.WriteLine(s);     
+
+
+
+
+
+
+
+
+
+
+
+
