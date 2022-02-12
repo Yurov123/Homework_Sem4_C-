6d590ad16d9859ce9af74050395c1bfc9e30e406
Konstantin <constantin8271@gmail.com>
2022-02-12 11:22:34 +0300

    task_30

---
+++ Task_30/Program.cs
@@ -0,0 +1,35 @@
+﻿//  Покажите кубы чисел, заканчивающихся на чётную цифру.
+
+Console.Clear();
+Console.Write("Enter the number of numbers that have cubes of numbers ending in an even digit = ");
+int N = int.Parse(Console.ReadLine());
+int count = 0;
+int i = 2;
+
+Console.WriteLine();
+Console.Write("Cubes of numbers ending in an even number: ");
+while (count < N)
+{   
+    if ((i*i*i) % 2 == 0)
+    {
+        count++;
+        Console.Write(i*i*i + " ");
+        i++;
+    }
+    else i++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
