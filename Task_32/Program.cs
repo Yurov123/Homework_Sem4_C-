3368b93fb6fc2bf914e0e27ca4683482873a575b
Konstantin <constantin8271@gmail.com>
2022-02-12 18:07:04 +0300

    task_32

---
+++ Task_32/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Задайте массив из восьми элементов, заполненных нулями и единицами. Выведите их на экран
+
+var numbers = new int[8];
+numbers[0] = 0;
+numbers[1] = 1;
+numbers[2] = 0;
+numbers[3] = 1;
+numbers[5] = 1;
+numbers[6] = 1;
+numbers[7] = 0;
+
+Console.WriteLine(string.Join(", ", numbers));
+
+
